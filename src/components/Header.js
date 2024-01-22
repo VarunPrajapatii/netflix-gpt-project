a086b4b1443735bb5479098102e98c129109cd5a
Varun Prajapati <varunprajapati123@gmail.com>
2024-01-23 02:33:12 +0530

    small bug of reducer fixed and profileURL error fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,24 +20,25 @@ const Header = () => {
   };
 
   return (
-    <div className='w-full absolute px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between'>
+    <div className='w-screen absolute px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between'>
       <img 
         className='w-44'
         alt="logo" 
         src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'
       />
-      
-      <div className='flex p-2'>
-        <img 
-          className='w-12 h-12'
-          alt='usericon'
-          src= {user?.photoURL}
-        />
-        <button 
-          className='font-bold text-white'
-          onClick={handleSignOut}
-        >(Sign Out)</button>
-      </div>
+      {user && (
+        <div className='flex p-2'>
+          <img 
+            className='w-12 h-12'
+            alt='usericon'
+            src= {user?.photoURL}
+          />
+          <button 
+            className='font-bold text-white'
+            onClick={handleSignOut}
+          >(Sign Out)</button>
+        </div>
+      )}
     </div>
   );
 };
