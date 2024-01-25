a2364816f5271f16fff126e1454645fb7be5b3e1
Varun Prajapati <varunprajapati123@gmail.com>
2024-01-25 15:35:49 +0530

    Added Movies Lists of Now Playing, Popular, Upcoming, TopRated in Secondary Container

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -6,11 +6,12 @@ const MovieList = ({title, movies}) => {
     return (
     <div className='px-6 text-white'>
       <h1 className='text-3xl py-4'>{title}</h1>
-      <div className='flex overflow-x-scroll'>
+      <div className='flex overflow-x-scroll no-scrollbar '>
         <div className='flex'>
           {movies?.map(movie => <MovieCard key={movie.id} posterPath={movie.poster_path} /> )}          
         </div>
       </div>
+        <button className='z-30 text-2xl bg-white text-black '>More</button>
     </div>
   );
 };
--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -12,9 +12,9 @@ const SecondaryContainer = () => {
       <div className='bg-black'>
         <div className='-mt-96 relative z-20'>
           <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
-          <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
-          <MovieList title={"Popular"} movies={movies.nowPlayingMovies} />
-          <MovieList title={"Upcoming Movies"} movies={movies.nowPlayingMovies} />
+          <MovieList title={"Popular"} movies={movies.popularMovies} />
+          <MovieList title={"Top Rated"} movies={movies.topRatedMovies} />
+          <MovieList title={"Upcoming Movies"} movies={movies.upcomingMovies} />
           <MovieList title={"Horror"} movies={movies.nowPlayingMovies} />
         </div>
         {/**
